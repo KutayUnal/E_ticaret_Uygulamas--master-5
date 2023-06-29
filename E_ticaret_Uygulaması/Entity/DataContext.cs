4303e61e3a222bf97fe82bf743b8cbf6575dbd52
KutayUnal <119403349+KutayUnal@users.noreply.github.com>
2023-06-29 10:16:36 +0300

    E_ticaret_Uygulamasmaster5

---
+++ E_ticaret_Uygulaması/Entity/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace E_ticaret_Uygulaması.Entity
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("dataConntection")
+        {
+            Database.SetInitializer(new DataInitializer());
+            //bu initializeri devreye sokuyoruz
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
